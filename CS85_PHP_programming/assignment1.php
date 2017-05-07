3e84c56c14994a0a009d4c85403579d10df74cec
Rahisul Haque <rhaque@localhost.localdomain>
2017-05-07 01:18:46 -0700

    adding all class project files

---
+++ CS85_PHP_programming/assignment1.php
@@ -0,0 +1,218 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Assignment 1-1</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="jumbotron">
+    <div class="containter">
+        <h1 align="middle"> This is CS 85.</h1>
+    </div>
+</div>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand">Homework assignments</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Week 1
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="http://smccs85.com/~rhaque/unit1-1/unit1-1.html#">Assignment 1-1</a></li>
+                    <li><a href="http://smccs85.com/~rhaque/unit1-2/assignment1#">Assignment 1-2</a></li>
+
+                </ul>
+            </li><li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Week 2
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Page 1-1</a></li>
+                    <li><a href="#">Page 1-2</a></li>
+                    <li><a href="#">Page 1-3</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Week 3
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Page 1-1</a></li>
+                    <li><a href="#">Page 1-2</a></li>
+                    <li><a href="http://smccs85.com/~rhaque/test1/test1.php">Exam 1</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Week 4
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Page 1-1</a></li>
+                    <li><a href="#">Page 1-2</a></li>
+                    <li><a href="#">Page 1-3</a></li>
+                </ul>
+            </li><li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Week 5
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Page 1-1</a></li>
+                    <li><a href="#">Page 1-2</a></li>
+                    <li><a href="#">Page 1-3</a></li>
+                </ul>
+            </li><li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Week 6
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Page 1-1</a></li>
+                    <li><a href="#">Page 1-2</a></li>
+                    <li><a href="#">Page 1-3</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Week 7
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Page 1-1</a></li>
+                    <li><a href="#">Page 1-2</a></li>
+                    <li><a href="#">Page 1-3</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Week 8
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Page 1-1</a></li>
+                    <li><a href="#">Page 1-2</a></li>
+                    <li><a href="#">Page 1-3</a></li>
+                </ul>
+            </li><li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Week 9
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Page 1-1</a></li>
+                    <li><a href="#">Page 1-2</a></li>
+                    <li><a href="#">Page 1-3</a></li>
+                </ul>
+            </li><li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Week 10
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Page 1-1</a></li>
+                    <li><a href="#">Page 1-2</a></li>
+                    <li><a href="#">Page 1-3</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Week 11
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Page 1-1</a></li>
+                    <li><a href="#">Page 1-2</a></li>
+                    <li><a href="#">Page 1-3</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Week 12
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Page 1-1</a></li>
+                    <li><a href="#">Page 1-2</a></li>
+                    <li><a href="#">Page 1-3</a></li>
+                </ul>
+            </li>
+
+        </ul>
+    </div>
+</nav>
+
+<div class="container">
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: rahisul
+     * Date: 3/29/17
+     * Time: 6:34 PM
+     */
+    $fact= array();
+    $fact[0]="Initially written as a set of Common Gateway Interface (CGI) in ‘C’ (1994).";
+    $fact[1]="PHP originally stood for \"Personal Home Page\".";
+    $fact[2]="Rasmus Lerdorf designed to replace a set of Perl scripts to maintain his Personal Home Pages (also known as PHP).";
+    $fact[3]="Some of the biggest online brands, such as Facebook, ProProfs, Digg, Friendster, Flickr, Technorati, and Yahoo! are powered by PHP.";
+    $fact[4]="There are about 5 million PHP developers worldwide.";
+    $fact[5]="75% of Web 2.0 sites are built in PHP. PHP is used by 81.7% of all the websites whose server-side programming language we know.";
+    $fact[6]="There are about 5 million PHP developers worldwide.";
+    $fact[7]="PHP is installed on over 20 million websites and 1 million web servers. (PHP: 244M sites, 2.1M IP addresses).";
+    $fact[8]="It is possible to use PHP in almost every operating system. PHP can be used in all major operating systems including Linux, Microsoft Windows, Mac OS X, and RISC OS.";
+    $fact[9]="PHP’s mascot is a big blue elephant that’s aptly named elePHPant.";
+    $fact[10]="PHP 7 was originally dubbed “PHP Next Generation (phpng)\".";
+    $fact[11]="Although Free, PHP is Not Distributed Under the General Public License";
+    $fact[12]="PHP was originally designed to create dynamic and more interactive web pages. It is the most widely-used, open-source and general-purpose scripting language.";
+    $fact[13]="PHP uses procedural programming or object oriented programming and also a mixture of them.";
+    $fact[14]="Zeev Suraski and Andi Gutmans, two developers at the Technion IIT, rewrote the parser in 1997 and formed the base of PHP 3.";
+    $i=rand(0,14);
+    echo"<h1>CS 85 and me!</h1>";
+    echo"<p>Hello, internet! In this class I am learnig PHP. It is a very popular server-side language. Though there are some new comers in the serverside scripting language, PHP is still strong for it's really well developed language.</p>";
+    echo"<h3>Here is a interesting fact about PHP: </h3>" ;
+    ?>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+
+            <?php
+            echo "<h2> $fact[$i]</h2>";
+            ?>
+        </div>
+        <h3> To know another fact, refresh the page!</h3>
+    </div>
+
+
+
+</div>
+<div class="footer">
+    <h6  align="center">All rights reserved. Created for the SMC-CS 85 class</h6>
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+<style>
+    .jumbotron{
+        font-family: "helvetica Neue";
+        color: white;
+        background-color: black;
+        margin-bottom: 0px;
+
+    }
+    .footer{
+        margin-top: 15%;
+    }
+    .navbar{
+        margin-top: 0px;;
+        margin-bottom: 0px;
+    }
+
+</style>
+
+</html>
